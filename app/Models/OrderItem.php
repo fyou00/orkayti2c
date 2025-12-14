6e8fcb472ceb51992b1045dd9905027966ae9042
Fathur <muhammadfathurrahman013@gmail.com>
2025-12-14 09:27:33 +0700

    update

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -36,31 +36,3 @@ class OrderItem extends Model
         });
     }
 }
-
-// app/Models/Transaction.php
-class Transaction extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'order_id',
-        'metode_pembayaran',
-        'status_pembayaran',
-        'total'
-    ];
-
-    public function order()
-    {
-        return $this->belongsTo(Order::class);
-    }
-
-    public function scopeLunas($query)
-    {
-        return $query->where('status_pembayaran', 'Lunas');
-    }
-
-    public function scopeBelumLunas($query)
-    {
-        return $query->where('status_pembayaran', 'Belum Lunas');
-    }
-}
