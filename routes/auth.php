6d7c250c5243f1ac69c70aa5119d550e60a6ffb8
fathur <muhammadfathurrahman013@gmail.com>
2026-06-12 18:29:37 +0700

    hapus yg ga penting

--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,4 @@
 <?php
-
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Support\Facades\Auth;
